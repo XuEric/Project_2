b552768f5a0ccb78bc1ecbc2c5b704fbfa314918
zhiliang.xzl@alibaba-inc.com <zhiliang.xzl@alibaba-inc.com>
2018-07-29 11:12:36 +0800

    submit

--- levelSandbox.js
+++ levelSandbox.js
@@ -7,32 +7,66 @@ const chainDB = './chaindata';
 const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
-function addLevelDBData(key,value){
-  db.put(key, value, function(err) {
-    if (err) return console.log('Block ' + key + ' submission failed', err);
-  })
+function addLevelDBData(key, value) {
+
+    db.put(key, value, function (err) {
+        if (err) return console.log('Block ' + key + ' submission failed', err);
+        console.log("key:" + key + ",value:" + value + " added");
+    })
 }
 
 // Get data from levelDB with key
-function getLevelDBData(key){
-  db.get(key, function(err, value) {
-    if (err) return console.log('Not found!', err);
-    console.log('Value = ' + value);
-  })
+function getLevelDBData(key) {
+    let v = undefined;
+    db.get(key, function (err, value) {
+        if (err) {
+            return console.log('Not found!', err);
+        }
+        console.log("key:" + key + ', Value = ' + value);
+        v = value;
+    });
+    return v;
 }
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
     let i = 0;
-    db.createReadStream().on('data', function(data) {
-          i++;
-        }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function() {
-          console.log('Block #' + i);
-          addLevelDBData(i, value);
-        });
+    db.createReadStream().on('data', function (data) {
+        i++;
+    }).on('error', function (err) {
+        return console.log('Unable to read data stream!', err)
+    }).on('close', function () {
+        console.log('Block #' + i);
+        addLevelDBData(i, value);
+    });
 }
+
+function getCount() {
+    for (let i = 0; ; i++) {
+        let v = getLevelDBData(i);
+        if (v == undefined) {
+            return i;
+        }
+    }
+
+    return undefined;
+}
+
+
+function dump() {
+    let i = 0;
+    console.log("dump---------------")
+    db.createReadStream().on('data', function (data) {
+        i++;
+        console.log("index:" + i + ",data:" + data);
+    }).on('error', function (err) {
+        return console.log('Unable to read data stream!', err)
+    }).on('close', function () {
+        console.log("dump-----------end")
+    });
+    return i;
+}
+
 
 /* ===== Testing ==============================================================|
 |  - Self-invoking function to add blocks to chain                             |
@@ -46,9 +80,18 @@ function addDataToLevelDB(value) {
 |  ===========================================================================*/
 
 
-(function theLoop (i) {
-  setTimeout(function () {
-    addDataToLevelDB('Testing data');
-    if (--i) theLoop(i);
-  }, 100);
-})(10);
+// (function theLoop (i) {
+//   setTimeout(function () {
+//     addDataToLevelDB('Testing data');
+//     if (--i) theLoop(i);
+//   }, 100);
+// })(1);
+
+// setTimeout(function () {
+//   getCount();
+// }, 1000);
+
+exports.put = addLevelDBData
+exports.get = getLevelDBData
+exports.length = getCount
+exports.dump = dump
--- simpleChain.js
+++ simpleChain.js
@@ -3,19 +3,21 @@
 |  =========================================================*/
 
 const SHA256 = require('crypto-js/sha256');
-
+const level = require('level');
+const chainDB = './chaindata';
+const db = level(chainDB);
 
 /* ===== Block Class ==============================
 |  Class with a constructor for block 			   |
 |  ===============================================*/
 
-class Block{
-	constructor(data){
-     this.hash = "",
-     this.height = 0,
-     this.body = data,
-     this.time = 0,
-     this.previousBlockHash = ""
+class Block {
+    constructor(data) {
+        this.hash = "",
+            this.height = 0,
+            this.body = data,
+            this.time = 0,
+            this.previousBlockHash = ""
     }
 }
 
@@ -23,76 +25,177 @@ class Block{
 |  Class with a constructor for new blockchain 		|
 |  ================================================*/
 
-class Blockchain{
-  constructor(){
-    this.chain = [];
-    this.addBlock(new Block("First block in the chain - Genesis block"));
-  }
+class Blockchain {
+    constructor() {
+        this.chain = db;
+        let i = 0;
+        let self = this;
+        this.chain.createReadStream().on('data', function (data) {
+            i++;
+            // console.log("index:" + i + ",data:" + data);
+        }).on('error', function (err) {
+            console.log('Unable to read data stream!', err)
+            process.exit(-1);
+        }).on('close', function () {
+            if (i == 0) {// this is the genesis block!
+                self.addBlock(new Block("First block in the chain - Genesis block"));
+            }
+        });
 
-  // Add new block
-  addBlock(newBlock){
-    // Block height
-    newBlock.height = this.chain.length;
-    // UTC timestamp
-    newBlock.time = new Date().getTime().toString().slice(0,-3);
-    // previous block hash
-    if(this.chain.length>0){
-      newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
-    }
-    // Block hash with SHA256 using newBlock and converting to a string
-    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-    // Adding block object to chain
-  	this.chain.push(newBlock);
-  }
-
-  // Get block height
-    getBlockHeight(){
-      return this.chain.length-1;
     }
 
-    // get block
-    getBlock(blockHeight){
-      // return object as a single string
-      return JSON.parse(JSON.stringify(this.chain[blockHeight]));
+    // Add new block
+    addBlock(newBlock) {
+
+        let i = 0;
+        let self = this;
+        this.chain.createReadStream().on('data', function (data) {
+            i++;
+            // console.log("index:" + i + ",data:" + data);
+        }).on('error', function (err) {
+            console.log('Unable to read data stream!', err)
+            process.exit(-1);
+        }).on('close', function () { // find the last block
+            // Block height
+            newBlock.height = i;
+            // UTC timestamp
+            newBlock.time = new Date().getTime().toString().slice(0, -3);
+            // previous block hash
+            if (i > 0) { // has previous block
+                db.get(i - 1, function (err, value) {
+                    if (err) {
+                        return console.log('previous block Not found!', err);
+                    }
+                    newBlock.previousBlockHash = JSON.parse(value).hash;
+
+                    // Block hash with SHA256 using newBlock and converting to a string
+                    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+
+                    // Adding block object to chain
+                    self.chain.put(i, JSON.stringify(newBlock), function (err) {
+                        if (err) return console.log('Block ' + i + ' submission failed', err);
+                        console.log("key:" + i + ",value:" + JSON.stringify(newBlock) + " added");
+                    });
+                });
+            } else { // genesis block
+                newBlock.previousBlockHash = "";
+
+                // Block hash with SHA256 using newBlock and converting to a string
+                newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+                // Adding block object to chain
+                self.chain.put(i, JSON.stringify(newBlock), function (err) {
+                    if (err) return console.log('Block ' + i + ' submission failed', err);
+                    console.log("key:" + i + ",value:" + JSON.stringify(newBlock) + " added");
+                });
+            }
+        });
     }
 
-    // validate block
-    validateBlock(blockHeight){
-      // get block object
-      let block = this.getBlock(blockHeight);
-      // get block hash
-      let blockHash = block.hash;
-      // remove block hash to test block integrity
-      block.hash = '';
-      // generate block hash
-      let validBlockHash = SHA256(JSON.stringify(block)).toString();
-      // Compare
-      if (blockHash===validBlockHash) {
-          return true;
+    dumpChain() {
+        console.log("dump---------------")
+        this.chain.createReadStream().on('data', function (data) {
+            console.log("key:" + data.key + ",data:" + JSON.stringify(data.value));
+        }).on('error', function (err) {
+            return console.log('Unable to read data stream!', err)
+        }).on('close', function () {
+            console.log("dump-----------end")
+        });
+    }
+
+// validate block
+    validateBlockHash(block) {
+        // get block hash
+        let blockHash = block.hash;
+        // remove block hash to test block integrity
+        block.hash = '';
+        // generate block hash
+        let validBlockHash = SHA256(JSON.stringify(block)).toString();
+        // Compare
+        if (blockHash === validBlockHash) {
+            return true;
         } else {
-          console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
-          return false;
+            console.log('Block #' + block.height + ' invalid hash:\n' + blockHash + '<>' + validBlockHash);
+            return false;
         }
     }
 
-   // Validate blockchain
-    validateChain(){
-      let errorLog = [];
-      for (var i = 0; i < this.chain.length-1; i++) {
-        // validate block
-        if (!this.validateBlock(i))errorLog.push(i);
-        // compare blocks hash link
-        let blockHash = this.chain[i].hash;
-        let previousHash = this.chain[i+1].previousBlockHash;
-        if (blockHash!==previousHash) {
-          errorLog.push(i);
-        }
-      }
-      if (errorLog.length>0) {
-        console.log('Block errors = ' + errorLog.length);
-        console.log('Blocks: '+errorLog);
-      } else {
-        console.log('No errors detected');
-      }
+// Validate blockchain
+    validateChain() {
+        let errorLog = [];
+
+        let self = this;
+        this.chain.createReadStream().on('data', function (data) {
+            let block = JSON.parse(data.value);
+
+            // validate hash
+            if (!self.validateBlockHash(block)) errorLog.push(block.height);
+
+            // validate previous hash
+            if (block.height > 0) {
+                db.get(block.height - 1, function (err, value) {
+                    if (err) {
+                        return console.log('previous block Not found!', err);
+                    }
+
+                    if (block.previousBlockHash != JSON.parse(value).hash) {
+                        errorLog.push(block.height);
+                    }
+                });
+            }
+        }).on('error', function (err) {
+            return console.log('Unable to read data stream!', err)
+        }).on('close', function () {
+            if (errorLog.length > 0) {
+                console.log('Block errors = ' + errorLog.length);
+                console.log('Blocks: ' + errorLog);
+            } else {
+                console.log('No errors detected');
+            }
+        });
     }
+}
+
+
+let bc = new Blockchain();
+
+
+// testBuildBlock();
+testValidation();
+
+function testBuildBlock() {
+    (function theLoop(i) {
+        setTimeout(function () {
+            bc.addBlock(new Block("test block"));
+            bc.dumpChain();
+            bc.validateChain();
+            if (--i) theLoop(i);
+        }, 500);
+    })(10);
+}
+
+
+function testValidation() {
+    let inducedErrorBlocks = [2, 4, 7];
+
+    for (var i = 0; i < inducedErrorBlocks.length; i++) {
+        let k = inducedErrorBlocks[i];
+
+        bc.chain.get(k, function (err, value) {
+            if (err) {
+                return console.log('previous block Not found!', err);
+            }
+
+            let block = JSON.parse(value);
+
+            block.body = 'induced chain error';
+            let v = JSON.stringify(block);
+            bc.chain.put(k, v, function (err) {
+                if (err) return console.log('Block ' + k + ' submission failed', err);
+                console.log("key:" + k + ",value:" + v + " added");
+            })
+
+        });
+    }
+
+    bc.validateChain();
 }
